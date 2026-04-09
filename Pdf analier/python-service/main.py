e1517e544ea8eb713de4300ae07c3ebc0b4bf3b9
Shaurya <shauryapandey4136@gmail.com>
2026-04-09 20:38:12 +0530

    added requirment file

--- Pdf analier/python-service/main.py
+++ Pdf analier/python-service/main.py
@@ -3,8 +3,14 @@ import re
 import fitz
 import spacy
 
+
 app = FastAPI()
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # ---------------- TEXT EXTRACTION ----------------
 def extract_text(file_bytes):
